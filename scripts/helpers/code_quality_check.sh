af48c59f11c3e7625c14776f9f3c40ee15a0aed0
Regina Leis <128072671+reginaleis@users.noreply.github.com>
2026-05-04 13:43:59 +0200

    Fix: line length in pipeline

--- scripts/helpers/code_quality_check.sh
+++
@@ -1,9 +0,0 @@
-#! bin/bash
-# This file contains all checkers that are used in pipelines.
-# Use locally to run all checkers at once, e.g., before pushing code to the repository.
-# Usage: bash local_checkers.sh
-
-poetry run isort --check --diff --color --line-length 79 src
-poetry run flake8 src
-poetry run black --check --diff --color src
-poetry run mypy src --show-error-codes --pretty --install-types --non-interactive
